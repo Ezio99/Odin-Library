3e86e22581881625bb28b261b48aa444d6a65976
Ezio99 <vaibhavritam@gmail.com>
2024-06-22 16:53:25 +0800

    Read status function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let myLibrary = [new Book('Robin hood'), new Book('The three body problem'), new Book('God is a gamer')];
+// [new Book('Robin hood'), new Book('The three body problem'), new Book('God is a gamer')];
+let myLibrary = new Map()
+
+myLibrary.set('Robin hood',new Book('Robin hood'))
+myLibrary.set('The three body problem',new Book('The three body problem'))
+myLibrary.set('God is a gamer',new Book('God is a gamer'))
 
 let cards = document.querySelector('.cards');
 const showFormButton = document.querySelector('#ShowForm');
@@ -19,34 +24,82 @@ addBookButton.addEventListener('click', addBookToLibrary)
 
 function Book(name){
     this.name = name;
+    this.readStatus = false;
+
     this.getName = function (){
         return name
     };
+}
+
+function toggleRead(e){
+    const bookName = e.target.parentNode.parentNode.id
+    console.log(e)
+    let parent = document.getElementById(`${bookName}`)
+
+    myLibrary.get(bookName).readStatus = !myLibrary.get(bookName).readStatus
+
+    parent.querySelector('#ReadStatus').textContent = `Finished Reading : ${myLibrary.get(bookName).readStatus}`
+
 }
 
 function addBookToLibrary(e){
     e.preventDefault();
     const BookName = dialog.querySelector("#Bookname").value
     const book = new Book(BookName)
-    myLibrary.push(book);
+    myLibrary.set(BookName,book);
     cards.appendChild(createCard(book))
-    console.log(myLibrary)
     dialog.close()
+}
+
+function removeBookFromLibrary(e){
+    console.log(e)
+    console.log(e.target.parentNode.id)
+    console.log(`#${e.target.parentNode.parentNode.id}`)
+
+
+    myLibrary.delete(`${e.target.parentNode.parentNode.id}`)
+    console.log(myLibrary)
+    document.getElementById(`${e.target.parentNode.parentNode.id}`).remove()
+
 }
 
 function createCard(book){
     const card = document.createElement('div');
     const p = document.createElement('p');
-    p.textContent = book.getName();
+    const readStatus = document.createElement('p');
+    const removeButton = document.createElement('button');
+    const readButton = document.createElement('button');
+    const cardButtons = document.createElement('div');
+
     card.classList.add('card');
+    card.id = book.getName();
+
+    cardButtons.classList.add('cardButtons');
+
+    p.textContent = book.getName();
+    readStatus.textContent = `Finished Reading : ${book.readStatus}`;
+    readStatus.id="ReadStatus"
+
+    removeButton.textContent="Remove"
+    removeButton.addEventListener("click", removeBookFromLibrary);
+
+    readButton.textContent="Toggle Read"
+    readButton.addEventListener("click",toggleRead)
+
     card.appendChild(p);
+    card.appendChild(readStatus)
+
+    cardButtons.appendChild(removeButton)
+    cardButtons.appendChild(readButton)
+
+    card.appendChild(cardButtons)
     
     return card
 }
 
 function displayCards(){
-    for (const book of myLibrary) {
-        cards.appendChild(createCard(book))
+    for (const [key,value] of myLibrary) {
+        cards.appendChild(createCard(value))
     }
 }
 
